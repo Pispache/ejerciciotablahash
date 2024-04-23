3ac68998d934ac9644e8b18930bd41400ac6e870
pispachea <pispachea@miumg.edu.gt>
2024-04-22 21:43:35 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/ejerciciotablahash/Ejerciciotablahash.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciotablahash;
+
+/**
+ *
+ * @author pabloispache
+ */
+public class Ejerciciotablahash {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
